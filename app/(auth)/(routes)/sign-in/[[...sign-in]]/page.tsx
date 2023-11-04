8df33b074c5c5bbbdb6dd413f3837fdfd529e49b
Adias Afnan Valentino <adiasafnan@gmail.com>
2023-11-04 18:23:48 +0700

    auth clerk

---
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="h-full flex justify-center items-center bg-slate-400">
+      <SignIn />
+    </div>
+  )
+}
